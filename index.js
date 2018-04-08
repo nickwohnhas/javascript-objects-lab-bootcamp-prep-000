73595caa06435f58680e83f6c438ca3485227dc9
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-08 02:03:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function deleteFromObjectByKey(object,key){
   return newobject
 }
 function destructivelyDeleteFromObjectByKey(object,key){
-  delete object.key
+  delete object[key]
   return object
 }
